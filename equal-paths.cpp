a3298ddd56d71c8d2bb1ff2e718ef0873853294b
Alice Wei <alicewei@usc.edu>
2023-03-22 23:43:44 +0000

    Recrusion Finished

--- equal-paths.cpp
+++ equal-paths.cpp
@@ -9,10 +9,76 @@ using namespace std;
 
 // You may add any prototypes of helper functions here
 
+bool TwoareEqual (Node* L, Node* R)
+{
+    // check if they are leave:
+    if (L->left == nullptr && L->right == nullptr)
+    {
+        return (R->left == nullptr && R->right == nullptr);
+    }
+    if (R->left == nullptr && R->right == nullptr)
+    {
+        return (L->left == nullptr && L->right == nullptr);
+    }
+
+    //Other Cases
+
+    if (L->left != nullptr)
+    {
+        if (L->right != nullptr)
+        {
+            if (!TwoareEqual(L->left, L->right)) return false;
+        }
+        if (R->left != nullptr)
+        {
+            if (!TwoareEqual(L->left, R->left)) return false;
+        }
+        else 
+        {
+            if (!TwoareEqual(L->left, R->right)) return false;
+        }
+    }
+    else //L->right exist L->left does not
+    {
+        if (R->left != nullptr)
+        {
+            if (!TwoareEqual(L->right, R->left)) return false;
+        }
+        else 
+        {
+            if (!TwoareEqual(L->right, R->right)) return false;
+        }
+    }
+    if (R->left != nullptr && R->right != nullptr)
+    {
+        if (!TwoareEqual(R->left, R->right)) return false;
+    }
+    return true;
+}
 
 bool equalPaths(Node * root)
 {
-    // Add your code below
+    if (root == nullptr)
+    {
+        return true;
+    }
+
+    if (root->left == nullptr && root->right == nullptr)
+    {
+        return true;
+    }
+    else if (root->left == nullptr)
+    {
+        return equalPaths(root -> right);
+    }
+    else if (root->right == nullptr)
+    {
+        return equalPaths(root->left);
+    }
+    else //have two children
+    {
+        return TwoareEqual(root->left, root->right);
+    }
 
 }
 
